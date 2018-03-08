782e1d2175ed582985d4d6b7107b3a2b4f9b4de0
Aditya <linkadity29@gmail.com>
2018-03-09 01:00:45 +0530

    Demo working

--- app/src/main/java/com/example/linka/donor/RegisterForm.java
+++ app/src/main/java/com/example/linka/donor/RegisterForm.java
@@ -12,19 +12,24 @@ import android.widget.Spinner;
 public class RegisterForm extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     RadioGroup rg;
     RadioButton rb;
+    String gen = "null";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_form);
-        rg = (RadioGroup) findViewById(R.id.rbGroup);
+        rg = findViewById(R.id.rbGroup);
         Spinner spinner = findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.blood_group, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
     }
-
+    public void rbClick (View view){
+        int radioButtonId = rg.getCheckedRadioButtonId();
+        rb = findViewById(radioButtonId);
+        gen = (String)rb.getText();
+    }
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
